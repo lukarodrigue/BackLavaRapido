34ae8672bb4d6b30b32639d65bf36fb24987791e
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-09-24 23:01:51 -0300

    🐞 fix(renamed route file name):
    the name of the routes folder was changed

--- src/Router/temp.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const AppointmentsController = require('../controllers/appointmentsController');
-
-const apptRoutes = Router();
-apptRoutes.post('/', AppointmentsController.create);
-apptRoutes.get('/:id', AppointmentsController.show);
-apptRoutes.delete('/:id', AppointmentsController.delete);
-module.exports = apptRoutes;
