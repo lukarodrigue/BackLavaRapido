3bf81fa1c28b087638388d7f6d752ed57720160d
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-09-29 00:15:03 -0300

    ✨ feat(created authConfig and AppError):
    an authentication configuration module and/or error handling was created

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const AppError = require('./utils/AppError');
 const express = require('express');
 const app = express();
 const database = require('./database/sqlite3');
@@ -10,6 +11,14 @@ const routes = require('./router/index');
 app.use(routes);
 
 database();
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  } 
+  console.error(error);
+  return response.status(500).json({ message: 'Internal server error' });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
