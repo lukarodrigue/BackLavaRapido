4033331ef0a404a925c584bfa8d129205a476efc
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-09-29 23:47:00 -0300

    ✨ feat(permission authentication middleware ):
    permission authentication middleware has been added

--- src/Router/appointments.routes.js
+++ src/Router/appointments.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const AppointmentsController = require('../controllers/appointmentsController');
 const ensureAuthenticated = require('../middleware/ensureAuthenticated');
+const checkAdminPermission = require('../middleware/checkAdminPermission');
 const apptRoutes = Router();
 
 const appointmentsController = new AppointmentsController();
 
 apptRoutes.post('/', appointmentsController.create);
 apptRoutes.get('/:id', ensureAuthenticated, appointmentsController.show);
-apptRoutes.delete('/:id', ensureAuthenticated, appointmentsController.delete);
-apptRoutes.put('/:id', ensureAuthenticated, appointmentsController.update);
+apptRoutes.delete('/:id', ensureAuthenticated, checkAdminPermission, appointmentsController.delete);
+apptRoutes.put('/:id', ensureAuthenticated, checkAdminPermission, appointmentsController.update);
 
 module.exports = apptRoutes;
--- src/Router/users.routes.js
+++ src/Router/users.routes.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const UsersController = require('../controllers/usersController');
 const ensureAuthenticated = require('../middleware/ensureAuthenticated')
-
+const checkAdminPermission = require('../middleware/checkAdminPermission');
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
 
 usersRoutes.post('/', usersController.create);
-usersRoutes.put('/:id', ensureAuthenticated, usersController.update);
+usersRoutes.put('/:id', ensureAuthenticated, checkAdminPermission, usersController.update);
 
 module.exports = usersRoutes;
